92d53627589d4e45b3f5ade5654b17c36352cf56
khalil kraiem <khalilmedkraiem@gmail>
2021-12-07 11:27:22 +0100

    function

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ function allowDrop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
-var button=$("#ok")
-button.click(function () { 
-  $("td").empty()
+// var button=$("#ok")
+// button.click(function () { 
+//   $("td").empty()
   
-});
-if ($(#td1).val()=$(#drag1)&&$(#td2).val()=$(#drag2)&&$(#td3).val()=$(#drag3)&&$(#td4).val()=$(#drag4)&&$(#td5).val()=$(#drag5)&&$(#td6).val()=$(#drag6)&&$(#td7).val()=$(#drag7)&&$(#td18).val()=$(#drag8)&&$(#td9).val()=$(#drag9)){
-  $("p").text()="you win"
-}else
-$("p").text()="good luck"
-}
+// });
+// if ($(#td1).val()=$(#drag1)&&$(#td2).val()=$(#drag2)&&$(#td3).val()=$(#drag3)&&$(#td4).val()=$(#drag4)&&$(#td5).val()=$(#drag5)&&$(#td6).val()=$(#drag6)&&$(#td7).val()=$(#drag7)&&$(#td18).val()=$(#drag8)&&$(#td9).val()=$(#drag9)){
+//   $("p").text()="you win"
+// }else
+// $("p").text()="good luck"
+// }
